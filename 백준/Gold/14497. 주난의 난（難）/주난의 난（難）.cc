38892adcbdad14566614bffa70ebfcae4cbaa495
EunWoo <162898956+eunwoo-levi@users.noreply.github.com>
2024-03-24 22:41:46 +0900

    Update 주난의 난（難）.cc

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.cc
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.cc
@@ -58,3 +58,66 @@ int main(){
     cout << res;
     
 }
+
+/* 풀이 2
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m;
+int a,b,c,d, res, flag=0;
+char A[304][304];
+int visited[304][304];
+const int dy[4] = {-1,0,1,0} , dx[4] = {0,1,0,-1};
+queue<pair<int,int>> Q, Q2;
+
+void bfs(int y, int x){
+    visited[y][x] = 1;
+    Q.push({y,x});
+
+    while(1){    
+        res++;
+
+        while(!Q.empty()){
+            tie(y,x) = Q.front();
+            Q.pop();
+
+            for(int i=0; i<4;i++){
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+
+                if(ny<1 || ny >n|| nx<1 || nx >m || visited[ny][nx])   continue;
+                visited[ny][nx] = 1;
+                
+                if(A[ny][nx]=='#') return;
+
+
+                if(A[ny][nx] == '1'){
+                    Q2.push({ny,nx});
+                    A[ny][nx] = '0';
+                }
+                else if(A[ny][nx] == '0')    Q.push({ny,nx});
+            }
+        }
+        Q = Q2;
+    }
+}
+
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> m;
+
+    cin >> a >> b >> c >> d;        // 주난 위치: a,b / 범인 위치 : c,d
+
+    for(int i=1; i<=n;i++){
+        for(int j=1; j<=m; j++){
+            cin >> A[i][j];
+        }
+    }
+
+    bfs(a,b);
+
+    cout << res;
+    
+}
+*/
