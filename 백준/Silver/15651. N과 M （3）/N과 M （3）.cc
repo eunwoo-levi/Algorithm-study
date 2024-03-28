f8c12ba3d33a7e2cb41375eda934f788da7318f7
EunWoo <162898956+eunwoo-levi@users.noreply.github.com>
2024-03-28 19:30:57 +0900

    [Silver III] Title: N과 M (3), Time: 456 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m;
+int arr[10];
+
+void solve(int idx,int len){
+    if(len==m){
+        for(int i=0;i<m;i++)
+            printf("%d ",arr[i]);
+        printf("\n");
+        return;
+    }
+
+    for(int i=1;i<=n;i++){
+        arr[len] = i;
+        solve(i,len+1);
+    }
+
+    return;
+}
+
+int main(){
+    scanf("%d %d",&n,&m);
+
+    solve(1,0);
+
+    return 0;
+}
