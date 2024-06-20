b1a5d5f9b09a3013447e203d516ba99f232d30a6
EunWoo <eunwoo1341@gmail.com>
2024-06-20 17:01:11 +0900

    [Silver V] Title: 수들의 합 5, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.cc
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.cc
@@ -1,14 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n,l=1,r=1,sum,ans;
-int main(){
-    cin >> n;
-    while(l <= r && r <= n) {
-        if(sum < n) sum += r++;
-        else{
-            if(sum == n) ans++;
-            sum -= l++;
-        }
-    }
-    cout << ans + 1;
+
+int n, Sum = 0, res = 1;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n;
+
+	int s = 1, e = 1;
+
+	while (e <= n)
+	{
+		if (Sum == n)
+		{
+			res++;
+			Sum += e;
+			e++;
+		}
+
+		else if (Sum < n)
+		{
+			Sum += e;
+			e++;
+		}
+
+		else
+		{
+			Sum -= s;
+			s++;
+		}
+	}
+
+	cout << res;
 }
