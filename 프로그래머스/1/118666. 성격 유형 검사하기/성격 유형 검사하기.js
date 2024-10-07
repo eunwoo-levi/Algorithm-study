f10bf2a117ba916ee2ec271ca44000c0f254346f
EunWoo <eunwoo1341@gmail.com>
2024-10-07 13:48:35 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.56 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,28 +1,16 @@
 function solution(survey, choices) {
-    const types = {
-        'R': 0, 'T': 0,
-        'C': 0, 'F': 0,
-        'J': 0, 'M': 0,
-        'A': 0, 'N': 0
-    };
-    
-    for (let i = 0; i < survey.length; i++) {
-        const [disagree, agree] = survey[i].split('');
-        const score = choices[i] - 4;
-        
-        if (score < 0) {
-            types[disagree] -= score;
-        } else if (score > 0) {
-            types[agree] += score;
-        }
-    }
-    
-    const result = [
-        types['R'] >= types['T'] ? 'R' : 'T',
-        types['C'] >= types['F'] ? 'C' : 'F',
-        types['J'] >= types['M'] ? 'J' : 'M',
-        types['A'] >= types['N'] ? 'A' : 'N'
-    ];
-    
-    return result.join('');
+    const MBTI = {};
+    const types = ["RT","CF","JM","AN"];
+
+    types.forEach((type) =>
+        type.split('').forEach((char) => MBTI[char] = 0)
+    )
+
+    choices.forEach((choice, index) => {
+        const [disagree, agree] = survey[index];
+
+        MBTI[choice > 4 ? agree : disagree] += Math.abs(choice - 4);
+    });
+
+    return types.map(([a, b]) => MBTI[b] > MBTI[a] ? b : a).join("");
 }
