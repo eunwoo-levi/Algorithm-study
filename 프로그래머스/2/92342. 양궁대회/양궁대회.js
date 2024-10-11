c25b0557fe34dde5f8f1ec86ce974fb8885ef7a7
EunWoo <eunwoo1341@gmail.com>
2024-10-11 23:21:06 +0900

    [level 2] Title: 양궁대회, Time: 1.61 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.js
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.js
@@ -1,54 +1,56 @@
 function solution(n, info) {
-  let max = 0;
-  let answer = [-1];
-  let ryan = Array(11).fill(0);       
-
-  function DFS(level, count) {
-    // 기저 조건(base case)
-    if (level == 11 || count == 0) {
-      if (count > 0) ryan[10] += count;       // 마지막에 남은 화살이 있다면 0점에 할당하고, 계산 후 다시 제거합니다.
-      
-      let sum = 0;
-      for (let i = 0; i < 11; i++) {
-        if (ryan[i] > info[i]) {              // ryan이 apeach보다 해당 level에서 더 많이 맞춘 경우
-          sum += (10 - i);
-        } 
-        else if (info[i] > 0) {               // 해당 level에서 ryan보다 더 많이 맞췄거나, 같은 화살 수인 경우
-          sum -= (10 - i);
+    var answer = [-1];
+    
+    let max = 0;
+    let ryan = new Array(info.length).fill(0)
+    
+    function dfs(level, arrow){
+        // 기저 조건
+        if(level===11 || arrow ===0){
+            if (arrow > 0) ryan[10] += arrow;
+            
+            let sum=0;
+            for(let i=0; i<info.length;i++){
+                if(ryan[i] > info[i]){
+                    sum+=10-i;
+                }
+                else if (info[i]){
+                    sum-=10-i;
+                }
+            }
+            
+            if(sum>0 && sum>max){
+                max = sum;
+                answer = [...ryan]
+            }
+            else if(sum>0 && sum===max){
+                for(let i=10; i>=0; i--){
+                   if(ryan[i]!==answer[i]){
+                       if(ryan[i]>answer[i]){
+                           max=sum;
+                           answer = [...ryan]
+                       }
+                       break;
+                   }
+                    
+                }
+            }
+            
+            if (arrow > 0) ryan[10] -= arrow; 
+            return;
         }
-      }
-      
-      if (sum > 0 && sum >= max) {
-        if (sum > max || isLowerScoreBetter(ryan, answer)) {
-          max = sum;
-          answer = [...ryan];
+        
+        
+        if(arrow>info[level]){
+            ryan[level]=info[level]+1;
+            dfs(level+1, arrow-ryan[level])
+            ryan[level] = 0;
         }
-      }
-      
-      if (count > 0) ryan[10] -= count;
-      return;
+        
+        dfs(level+1, arrow)
     }
     
-    // 
-    if (count > info[level]) {
-      ryan[level] = info[level] + 1;
-      DFS(level + 1, count - ryan[level]);
-      ryan[level] = 0;                        // 백트래킹  -  이전 단계로 되돌아감
-    }
+    dfs(0, n)
     
-    DFS(level + 1, count);
-  }
-  
-  function isLowerScoreBetter(ryan, answer) {
-    for (let i = 10; i >= 0; i--) {           // 가장 낮은 점수를 더 맞이 맞힌 경우 (문제 조건)
-      if (ryan[i] !== answer[i]) {
-        return ryan[i] > answer[i];
-      }
-    }
-    return false;
-  }
-  
-  DFS(0, n);
-  
-  return answer[0] === -1 ? [-1] : answer;
+    return answer;
 }
