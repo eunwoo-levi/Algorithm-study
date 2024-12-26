d0d478cde5c96ceab778b7fe2305e9c90fc4c86c
EunWoo <eunwoo1341@gmail.com>
2024-12-27 04:23:25 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 947.15 ms, Memory: 276 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.js
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.js
@@ -11,23 +11,23 @@ function solution(edges) {
     }
 
     let addedNode = 0;
-    let donutCnt = 0;
-    let lineCnt = 0;
-    let eightCnt = 0;
+    let donutGraph = 0;
+    let lineGraph = 0;
+    let eightGraph = 0;
 
     // 한 번만 순회하면서 결과 계산
     for (const [start, [given, received]] of Object.entries(map)) {
         if (given > 1 && received === 0) {
             addedNode = start; // addedNode 찾기
         } else if (given === 0) {
-            lineCnt++;  // 선이 끝나는 노드 카운트
+            lineGraph++;  // 선이 끝나는 노드 카운트
         } else if (given > 1 && received > 1) {
-            eightCnt++; // 팔 모양 노드 카운트
+            eightGraph++; // 팔 모양 노드 카운트
         }
     }
 
     // 도넛 노드 계산
-    donutCnt = map[addedNode][0] - lineCnt - eightCnt;
+    donutGraph = map[addedNode][0] - lineGraph - eightGraph;
 
-    return [Number(addedNode), donutCnt, lineCnt, eightCnt];
+    return [Number(addedNode), donutGraph, lineGraph, eightGraph];
 }
