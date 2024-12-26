b4532996b4c020881ec40bb98c6725a615ea213a
EunWoo <eunwoo1341@gmail.com>
2024-12-27 02:19:14 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 18.29 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,35 +1,26 @@
 function solution(friends, gifts) {
-    const n = friends.length
-    const giftPoints = new Array(n).fill(0)
-    const index = {}
-    const record = []
-    const points = new Array(n).fill(0)
-
-    for(let i=0;i<n;i++){
-        record[i]=new Array(n).fill(0)
-        index[friends[i]] = i
-    }
-
-    for(const gift of gifts){
-        const [giver, taker] = gift.split(' ')
-        record[index[giver]][index[taker]] +=1
-        giftPoints[index[giver]] +=1
-        giftPoints[index[taker]] -=1
-    } 
-
-    for(let i=0;i<n;i++){
-        for(let j=0;j<n;j++){
-            if(record[i][j]>record[j][i]){
-                points[i]+=1
-            } 
-            
-            else if(record[i][j]===record[j][i]){
-                if(giftPoints[i]>giftPoints[j]){
-                    points[i]+=1
-                }
-            }
-        }
-    } 
-     return Math.max(...points)
- }
- 
+   const n = friends.length
+   const giftPoints = new Array(n).fill(0)
+   const record = Array.from({length:friends.length}, ()=>Array(friends.length).fill(0))
+   const points = new Array(n).fill(0)
+   
+   for(const gift of gifts){
+       const [giver, taker] = gift.split(' ')
+       record[friends.indexOf(giver)][friends.indexOf(taker)] +=1
+       giftPoints[friends.indexOf(giver)] +=1
+       giftPoints[friends.indexOf(taker)] -=1
+   } 
+    
+   for(let i=0;i<n;i++){
+       for(let j=0;j<n;j++){
+           if(record[i][j]>record[j][i]){
+               points[i]+=1
+           } else if(record[i][j]===record[j][i]){
+               if(giftPoints[i]>giftPoints[j]){
+                   points[i]+=1
+               }
+           }
+       }
+   } 
+    return Math.max(...points)
+}
