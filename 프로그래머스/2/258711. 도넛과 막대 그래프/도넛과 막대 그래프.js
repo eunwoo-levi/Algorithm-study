5c48fe3b706991d94e26f05a03ee46aa3f110e81
EunWoo <eunwoo1341@gmail.com>
2024-12-27 03:18:01 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 1086.56 ms, Memory: 277 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.js
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.js
@@ -1,31 +1,33 @@
-function solution(edges) {    
-    const graphInfo = {}
+function solution(edges) {
+    const map = {}
 
-    edges.forEach(([send,receive])=>{
-        graphInfo[send] ||= { sendCount:0, receiveCount:0 }
-        graphInfo[receive] ||= { sendCount:0, receiveCount:0 }
-
-        graphInfo[send].sendCount++
-        graphInfo[receive].receiveCount++
-    })
-
-    let createVertex = 0
-    let cnt = 0
-    let donut = 0 
-    let bar = 0 
-    let eightChar = 0
-
-    for (let vertex in graphInfo){
-        const { sendCount, receiveCount } = graphInfo[vertex]
-        if(sendCount >= 2 && receiveCount === 0){
-            createVertex = +vertex
-            cnt = sendCount
-        }
-        if(sendCount == 0) bar++
-        if(sendCount >= 2 && receiveCount >= 2) eightChar++
+    // 1차원으로 각 노드의 출발 차수와 도착 차수를 한 번에 계산
+    for (const [start, end] of edges) {
+        if (!map[start]) map[start] = [0, 0];
+        if (!map[end]) map[end] = [0, 0];
+        
+        map[start][0]++;  // 출발 차수 증가
+        map[end][1]++;    // 도착 차수 증가
     }
 
-    donut = cnt - eightChar - bar
+    let addedNode = 0;
+    let donutCnt = 0;
+    let lineCnt = 0;
+    let eightCnt = 0;
 
-    return [createVertex, donut, bar, eightChar];
+    // 한 번만 순회하면서 결과 계산
+    for (const [start, [given, received]] of Object.entries(map)) {
+        if (given > 1 && received === 0) {
+            addedNode = start; // addedNode 찾기
+        } else if (given === 0) {
+            lineCnt++;  // 선이 끝나는 노드 카운트
+        } else if (given > 1 && received > 1) {
+            eightCnt++; // 팔 모양 노드 카운트
+        }
+    }
+
+    // 도넛 노드 계산
+    donutCnt = map[addedNode][0] - lineCnt - eightCnt;
+
+    return [Number(addedNode), donutCnt, lineCnt, eightCnt];
 }
