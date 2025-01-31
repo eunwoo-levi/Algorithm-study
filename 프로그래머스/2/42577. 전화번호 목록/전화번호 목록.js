a3893e38b8d9519a3552152dc4896b09b0e13fd8
EunWoo <eunwoo1341@gmail.com>
2025-01-31 16:12:09 +0900

    [level 2] Title: 전화번호 목록, Time: 126.30 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,12 @@
+function solution(phone_book) {
+    
+    phone_book.sort()
+    
+    for(let i=0; i<phone_book.length-1;i++){
+        if(phone_book[i+1].substring(0, phone_book[i].length)===phone_book[i]){
+            return false
+        }
+    }
+    
+    return true;
+}
