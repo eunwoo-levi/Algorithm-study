813b7e73ce2d97b0ea20068be304d49dec7f7b7f
EunWoo <eunwoo1341@gmail.com>
2025-02-06 21:57:15 +0900

    [level 2] Title: 방문 길이, Time: 1.29 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,35 +1,32 @@
-function check(nx, ny){
-    return ny>=-5 && ny<=5 && nx>=-5 && nx<=5
-}
-
-function update(x,y,dir){
-    switch(dir){
-        case "U":
-            return [x,y+1]
-        case "D":
-            return [x,y-1]
-        case "L":
-            return [x-1,y]
-        case "R":
-            return [x+1,y]
-    }
-}
-
 function solution(dirs) {
-    let x=0,y=0;
-    let visited = new Set()
+    const visited = new Set()
+    let x=0, y=0;
     
-    for(const i of dirs){
-        const [nx,ny] = update(x,y,i)
-        
-        if(!check(nx,ny))   continue;
-        
-        visited.add(`${x},${y},${nx},${ny}`);
-        visited.add(`${nx},${ny},${x},${y}`);
-        
-        [x,y] = [nx,ny]
+    
+    for(let i=0; i<dirs.length; i++){
+        const [nx, ny] = update(x,y, dirs[i])
+
+        if(nx>=-5 && nx<=5 && ny>=-5 && ny<=5){
+            visited.add(`${x},${y},${nx},${ny}`);
+            visited.add(`${nx},${ny},${x},${y}`);
+            [x, y] = [nx, ny]
+        }
     }
-    
     
     return visited.size/2;
+}
+
+function update(x,y, dir){
+    if(dir==='U'){
+        return [x,y+1]
+    }
+    else if(dir==='D'){
+        return [x, y-1]
+    }
+    else if(dir==='R'){
+        return [x+1 , y]
+    }
+    else{
+        return [x-1, y];
+    }
 }
