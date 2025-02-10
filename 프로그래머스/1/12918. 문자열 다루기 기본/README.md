7c4b114424c904fbc3b68ffbd15c2038aee48b18
EunWoo <eunwoo1341@gmail.com>
2025-02-11 00:54:41 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/README.md
+++ 프로그래머스/1/12918. 문자열 다루기 기본/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 다루기 기본 - 12918 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918) 
 
 ### 성능 요약
 
-메모리: 33.3 MB, 시간: 0.06 ms
+메모리: 33.6 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 13일 05:01:00
+2025년 02월 11일 00:54:36
 
 ### 문제 설명
 
--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,10 +1,15 @@
 function solution(s) {
-    
-    if(s.length!=4 && s.length!=6)    return false
-    
-    for(let i of s){
-        if(isNaN(i)) return false
+    if (s.length !== 4 && s.length !== 6) {
+        return false;
     }
     
-    return true
+    
+    for(let i=0; i<s.length;i++){
+        if(isNaN(s[i])){
+            return false
+        }
+    }
+    
+    
+    return true;
 }
