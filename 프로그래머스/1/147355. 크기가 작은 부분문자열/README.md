2b1d3754cdf415c1dd5df6508605dbb75f68132c
EunWoo <eunwoo1341@gmail.com>
2025-02-11 22:32:45 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 4.96 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/README.md
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크기가 작은 부분문자열 - 147355 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147355?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147355) 
 
 ### 성능 요약
 
-메모리: 36.8 MB, 시간: 4.54 ms
+메모리: 36.9 MB, 시간: 4.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 13일 17:46:50
+2025년 02월 11일 22:32:42
 
 ### 문제 설명
 
--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,11 +1,15 @@
 function solution(t, p) {
-    var answer = 0;
+    const res = [];
     
-    for(let i=0; i<=t.length-p.length;i++){
-        if(parseInt(t.substring(i,i+p.length))<=parseInt(p)){
-            answer++;
+    const len = p.length;
+    
+    for(let i=0; i<t.length - len + 1; i++){
+        const sliced = t.slice(i, i+len);
+        if(sliced <= p){
+            res.push(t.slice(i, i+len));
         }
     }
     
-    return answer;
+    
+    return res.length
 }
