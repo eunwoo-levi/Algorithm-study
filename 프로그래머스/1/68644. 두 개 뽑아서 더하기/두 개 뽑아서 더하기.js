677a119cb1897eaeaae9336ce5b5a0a53cb7c048
EunWoo <eunwoo1341@gmail.com>
2025-02-13 03:00:47 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.43 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,13 +1,13 @@
 function solution(numbers) {
-    var answer = new Set()
+    var res = [];
     
-    for(let i=0; i<numbers.length;i++){
-        for(let j=i+1; j<numbers.length;j++){
-            answer.add(numbers[i]+numbers[j])
+    const set = new Set();
+    
+    for(let i=0; i<numbers.length; i++){
+        for(let j=i+1; j<numbers.length; j++){
+            set.add(numbers[i]+numbers[j]);
         }
     }
     
-    answer = [...answer].sort((a,b)=>a-b)
-    
-    return answer;
+    return [...set].sort((a,b)=>a-b);
 }
