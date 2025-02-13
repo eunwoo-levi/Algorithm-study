5b9705043c8702aa791f62518b0c753757426c06
EunWoo <eunwoo1341@gmail.com>
2025-02-13 13:46:52 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.11 ms
+메모리: 33.6 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 11일 14:36:58
+2025년 02월 13일 13:46:47
 
 ### 문제 설명
 
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,16 +1,12 @@
 function solution(s) {
-    // 숫자 문자열과 해당 숫자를 매핑한 배열
-    const num = [
-        ["zero", 0], ["one", 1], ["two", 2], ["three", 3], ["four", 4],
-        ["five", 5], ["six", 6], ["seven", 7], ["eight", 8], ["nine", 9]
-    ];
+    
+    const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    
+    for(let i=0; i<numbers.length; i++){
+        let arr = s.split(numbers[i]);
+        s = arr.join(i)
+    }
 
-    // 매핑된 값을 순회하면서 문자열을 숫자로 변환
-    num.forEach(pair => {
-        // 각 문자열에 대해 replace를 이용해 숫자로 변환
-        s = s.replace(new RegExp(pair[0], 'g'), pair[1]);
-    });
-
-    // 숫자로 변환한 문자열을 숫자로 변환하여 반환
     return Number(s);
 }
+
