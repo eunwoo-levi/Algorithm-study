20dc59ef7f48d8e3565d4c162b89b94aa66e4097
EunWoo <eunwoo1341@gmail.com>
2025-03-20 23:53:06 +0900

    Time: 0 ms (100%), Space: 53.5 MB (30.08%) - LeetHub

---
+++ LeetCode/0027-remove-element/0027-remove-element.js
@@ -0,0 +1,11 @@
+var removeElement = function(nums, val) {
+    while(1){
+        const idx = nums.indexOf(val);
+        if(idx===-1){
+            break;
+        }
+
+        nums.splice(idx,1)
+    }
+
+};
