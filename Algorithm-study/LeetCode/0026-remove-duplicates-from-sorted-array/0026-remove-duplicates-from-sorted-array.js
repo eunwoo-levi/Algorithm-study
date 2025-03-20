904e3506be75b25a0e36c7295a91c05cd8148f92
EunWoo <eunwoo1341@gmail.com>
2025-03-20 23:30:59 +0900

    Time: 0 ms (100%), Space: 57.7 MB (29.01%) - LeetHub

---
+++ Algorithm-study/LeetCode/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,10 @@
+var removeDuplicates = function(nums) {
+    let uniqueIndex = 1;  // Start from the second element
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[uniqueIndex] = nums[i];
+            uniqueIndex++;
+        }
+    }
+    return uniqueIndex;
+};
