3cac9df862d5436c3dd1c71c6fb137ee4c598010
EunWoo <eunwoo1341@gmail.com>
2025-03-28 22:33:54 +0900

    [level 2] Title: 숫자 변환하기, Time: 1746.73 ms, Memory: 263 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,17 +1,10 @@
 function solution(x, y, n) {
-  var res = 0;
- 
-  var que = [[y, 0]];
- 
-  while (que.length) {
-    const [num, i] = que.shift();
- 
-    if (num === x) return i;
- 
-    if (num % 2 === 0 && num / 2 >= x) que.push([num / 2, i + 1]);
-    if (num % 3 === 0 && num / 3 >= x) que.push([num / 3, i + 1]);
-    if (num - n >= x) que.push([num - n, i + 1]);
+  const dp = new Array(y + 1).fill(Infinity);
+  dp[x] = 0;
+  for (let i = x; i <= y; i++) {
+    dp[i + n] = Math.min(dp[i + n], dp[i] + 1);
+    dp[i * 2] = Math.min(dp[i * 2], dp[i] + 1);
+    dp[i * 3] = Math.min(dp[i * 3], dp[i] + 1);
   }
- 
-  return -1;
+  return dp[y] !== Infinity ? dp[y] : -1;
 }
