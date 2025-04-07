75eace1c1d8db8445d4c451e41a8dea0dda52a0e
EunWoo <eunwoo1341@gmail.com>
2025-04-07 18:06:52 +0900

    [level 2] Title: 소수 찾기, Time: 5.32 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,31 +1,4 @@
 function solution(numbers) {
-    const numSet = new Set();
-    const visited = Array(numbers.length).fill(false);
-
-    const dfs = (path) => {
-        if (path.length > 0) {
-            numSet.add(Number(path));
-        }
-        if (path.length === numbers.length) return;
-
-        for (let i = 0; i < numbers.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                dfs(path + numbers[i]);
-                visited[i] = false;
-            }
-        }
-    };
-
-    dfs('');
-    
-    return [...numSet].filter(isPrime).length;
+    var answer = 0;
+    return answer;
 }
-
-const isPrime = (n) => {
-    if (n < 2) return false;
-    for (let i = 2; i * i <= n; i++) {
-        if (n % i === 0) return false;
-    }
-    return true;
-};
