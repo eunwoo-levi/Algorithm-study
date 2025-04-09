c2939e1b016a43307967b5c01e03fb23d0dfed12
EunWoo <eunwoo1341@gmail.com>
2025-04-09 16:57:27 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 14.98 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,32 +1,33 @@
 function solution(arr) {
-    const answer = [0, 0]; // [0의 개수, 1의 개수]
-
-    const compress = (x, y, size) => {
-        const first = arr[x][y];
+    const res = [0,0];
+    
+    const compress = (x,y, size)=>{
+        const first = arr[y][x];
         let same = true;
-
-        // 현재 영역이 전부 같은 숫자인지 확인
-        for (let i = x; i < x + size; i++) {
-            for (let j = y; j < y + size; j++) {
-                if (arr[i][j] !== first) {
+        
+        for(let i=y; i<y+size; i++){
+            for(let j=x; j<x+size; j++){
+                if(arr[i][j]!==first){
                     same = false;
                     break;
                 }
             }
-            if (!same) break;
+            if(!same)   break;
         }
-
-        if (same) {
-            answer[first]++;
-        } else {
-            const half = size / 2;
-            compress(x, y, half);                     // 왼쪽 위
-            compress(x, y + half, half);              // 오른쪽 위
-            compress(x + half, y, half);              // 왼쪽 아래
-            compress(x + half, y + half, half);       // 오른쪽 아래
+        
+        if(same){
+            res[first]++;
         }
-    };
-
-    compress(0, 0, arr.length);
-    return answer;
+        else{
+            const half = size/2;
+            compress(x,y,half);
+            compress(x+half,y,half);
+            compress(x,y+half,half);
+            compress(x+half, y+half, half);
+        }
+    }
+    
+    compress(0,0, arr.length)
+    
+    return res;
 }
