97f3a4924d2f160b90a1fb43eb3c28f11acf3113
EunWoo <eunwoo1341@gmail.com>
2025-06-16 09:30:23 +0900

    [level 1] Title: 약수의 합, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,10 +1,11 @@
-
 function solution(n) {
-    let res=0;
-    for(let i=1; i<=n;i++){
-        if(Math.floor(n%i)===0){
+    let res = 0;
+    
+    for(let i=1; i<=n; i++){
+        if(n%i===0){
             res+=i;
         }
     }
+    
     return res;
 }
