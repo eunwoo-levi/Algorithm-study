f921d1c6a042279ca791c8ef0673a558e11ea9f1
EunWoo <eunwoo1341@gmail.com>
2025-06-17 00:11:25 +0900

    [level 2] Title: 짝지어 제거하기, Time: 70.41 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,23 +1,13 @@
-function solution(s)
-{
-    const stack = []
+function solution(s) {
+  const stack = [];
 
-    for(let i=0; i<s.length; i++){
-        if(stack===0){
-            stack.push(s[i]);
-            continue;
-
-        }
-        if(stack.length!== 0 && stack[stack.length-1]===s[i]){
-            stack.pop();
-            continue;
-        }
-        stack.push(s[i])
+  for (let char of s) {
+    if (stack.length > 0 && stack[stack.length - 1] === char) {
+      stack.pop(); // 짝지어 제거
+    } else {
+      stack.push(char); // 스택에 추가
     }
+  }
 
-    if(stack.length===0){
-        return 1;
-    }
-    
-    return 0;
+  return stack.length === 0 ? 1 : 0;
 }
