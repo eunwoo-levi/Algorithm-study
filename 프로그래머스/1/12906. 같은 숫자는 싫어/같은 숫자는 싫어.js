310ca1f424ca1d0796ca098a4d4b02d40095c9e9
EunWoo <eunwoo1341@gmail.com>
2025-06-16 09:37:56 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 39.26 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -3,10 +3,9 @@ function solution(arr)
     const res = [arr[0]];
     
     for(let i=1; i<arr.length; i++){
-        if(arr[i-1]===arr[i]){
-            continue;
+        if(arr[i]!==res[res.length-1]){
+            res.push(arr[i]);
         }
-        res.push(arr[i]);
     }
     
     return res;
