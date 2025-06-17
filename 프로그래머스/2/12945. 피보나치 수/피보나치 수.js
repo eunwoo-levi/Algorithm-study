61490af0ba73ba1ceeaebafe02d7ee894517bb25
EunWoo <eunwoo1341@gmail.com>
2025-06-17 20:42:50 +0900

    [level 2] Title: 피보나치 수, Time: 6.40 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,10 +1,9 @@
 function solution(n) {
-    const mod = 1234567;
-    let dp = [0, 1, 1];
+    const dp = [0, 1, 1]
     
-    for (let i = 3; i <= n; i++) {
-        dp[i] = (dp[i - 1] + dp[i - 2]) % mod;
+    for(let i=3; i<=n; i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
     }
-
+    
     return dp[n];
 }
