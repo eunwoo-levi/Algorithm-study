0b0da0f5e2506277a9546ed4718f435d4c210e96
EunWoo <eunwoo1341@gmail.com>
2025-06-17 18:12:04 +0900

    [level 1] Title: 삼총사, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,12 +1,12 @@
 function solution(number) {
     let res = 0;
+    const n = number.length;
     
-    for(let i=0; i<number.length;i++){
-        for(let j=i+1; j<number.length; j++){
-            for(let k=j+1; k<number.length; k++){
-                if(number[i]+number[j]+number[k]===0){
+    for(let i=0; i<n; i++){
+        for(let j=i+1; j<n; j++){
+            for(let k=j+1; k<n; k++){
+                if(number[i] + number[j] + number[k]===0){
                     res++;
-                    
                 }
             }
         }
@@ -14,4 +14,3 @@ function solution(number) {
     
     return res;
 }
-
