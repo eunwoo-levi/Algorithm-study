c405d04382c47851e93f394b17197e10c72c0bcd
EunWoo <eunwoo1341@gmail.com>
2025-06-18 01:30:08 +0900

    [level 2] Title: 구명보트, Time: 40.14 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,18 +1,18 @@
+// greedy
 function solution(people, limit) {
-    people.sort((a, b) => a - b);
+    var answer = 0;
     
-    let start = 0; 
-    let end = people.length - 1;
-    let res = 0; 
+    people.sort((a,b)=>a-b);
+    let start=0, end=people.length-1;
     
-    while (start <= end) {
-        if (people[start] + people[end] <= limit) {
+    while(start<=end){
+        if(people[start]+people[end]<=limit){
             start++;
         }
-        // 무거운 사람은 무조건 보트를 타야 하므로 end만 이동
         end--;
-        res++;
+        answer++;
     }
     
-    return res;
+    
+    return answer;
 }
