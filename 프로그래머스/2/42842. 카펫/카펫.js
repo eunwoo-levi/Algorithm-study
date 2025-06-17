183744c7bd8c23734d29f871a8567a57f44373a1
EunWoo <eunwoo1341@gmail.com>
2025-06-17 20:48:40 +0900

    [level 2] Title: 카펫, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -4,18 +4,14 @@
 // 위에 식은 brown = (x+2)*(y+2) - yellow 랑 같다
 
 function solution(brown, yellow) {
-    
     for(let x=1; x<=yellow; x++){
         if(yellow%x===0){
             let y = yellow / x;
-            
             if(brown ===(x+2)*(y+2)-yellow){
-                return [y+2, x+2]
+                return [x+2, y+2].sort((a,b)=>b-a)
             }
         }
     }
-    
-    
     
     return [0,0];
 }
