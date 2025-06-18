baad138e9c21bd4353b91c65d4d81e9e13ba333f
EunWoo <eunwoo1341@gmail.com>
2025-06-19 00:51:39 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,12 +1,9 @@
+// dp
 function solution(n) {
-    const MOD = 1234567;
-    if (n === 1) return 1;
-    if (n === 2) return 2;
+    const dp=[0,1,2];
     
-    let dp = [0, 1, 2]; // dp[0]은 사용하지 않음
-    
-    for (let i = 3; i <= n; i++) {
-        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+    for(let i=3; i<=n;i++){
+        dp[i]= (dp[i-2] + dp[i-1]) % 1234567;
     }
     
     return dp[n];
