9b254da00fd40c37a68dd6b7e44eee77fea3b305
EunWoo <eunwoo1341@gmail.com>
2025-06-19 15:39:00 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1689.97 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,15 +1,14 @@
+// 원형 큐
 function solution(elements) {
-    let res = 0;
-    
-    const circularArr = elements.concat(elements)
-    const sumSet = new Set();
-    
-    for(let n=1; n<=elements.length; n++){
-        for(let i=0; i<elements.length;i++){
-            const sum = circularArr.slice(i, i+n).reduce((acc,cur)=>acc+cur, 0);
-            sumSet.add(sum);
+    const circular = elements.concat(elements);
+    const set = new Set();
+
+    for(let i=0; i<elements.length; i++){
+        for(let j=0; j<elements.length; j++){
+            const sum = circular.slice(i, i+j).reduce((acc,cur)=>acc+cur,0);
+            set.add(sum);
         }
     }
     
-    return sumSet.size
+    return set.size
 }
