72ac98f4c39a8e620971438a620280ac406f3e9f
EunWoo <eunwoo1341@gmail.com>
2025-06-20 19:17:45 +0900

    [level 1] Title: 기사단원의 무기, Time: 117.43 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,20 +1,21 @@
 function solution(number, limit, power) {
-  let answer = 0;
-
-  for (let i = 1; i <= number; i++) {
-    let divisor = 0;
-    for (let j = 1; j <= Math.sqrt(i); j++) {
-      if (i % j === 0) {
-        if (i / j === j) divisor += 1;
-        else divisor += 2;
-      }
-      if (divisor > limit) {
-        divisor = power;
-        break;
-      }
+    var answer = 0;
+    
+    for (let n = 1; n <= number; n++) {
+        let count = 0;
+        
+        for (let j = 1; j <= Math.sqrt(n); j++) {
+            if (n % j === 0) {  
+                count++; 
+                if (j !== n / j) {  // j와 n/j가 다르면 n/j도 약수
+                    count++;
+                }
+            }
+        }
+        
+        if (count > limit) count = power;
+        answer += count;
     }
-    answer += divisor;
-  }
-  
-  return answer;
+    
+    return answer;
 }
