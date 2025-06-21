e50c920a60c0e7625b8bc5fe17eb75d86211b828
EunWoo <eunwoo1341@gmail.com>
2025-06-21 17:28:35 +0900

    [level 1] Title: 카드 뭉치, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,22 +1,11 @@
 function solution(cards1, cards2, goal) {
-    let n=0, m=0;
-    
-    for(let i=0; i<goal.length; i++){
-        if(n>cards1.length || m>cards2.length){
-            console.log("?????????????")
-            return "No"
-        }
-        
-        if(cards1[n]===goal[i]){
-            n++;
-        }
-        else if(cards2[m]===goal[i]){
-            m++;
-        }
-        else{
-            return "No"
-        }
+    for(letter of goal){
+        if(cards1[0]===letter)  cards1.shift();
+        else if(cards2[0]===letter) cards2.shift();
+        else    return 'No'
     }
     
-    return "Yes"
+    
+    
+    return 'Yes'
 }
