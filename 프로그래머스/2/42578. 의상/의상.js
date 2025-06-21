d9b9ef565e904c40240ac5f979f74771f9332e57
EunWoo <eunwoo1341@gmail.com>
2025-06-21 19:38:09 +0900

    [level 2] Title: 의상, Time: 3.81 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,21 +1,18 @@
+// hash
 function solution(clothes) {
-    const map = new Map();
+    var answer = 1;
+    const map = {};
     
-    // 의상 카테고리별로 옷의 개수를 저장
-    for (let [item, category] of clothes) {
-        if (map.has(category)) {
-            map.set(category, map.get(category) + 1);
-        } else {
-            map.set(category, 1);
-        }
-    }
-
-    // 조합을 계산 (각 카테고리에서 "옷을 고르거나 안 고르거나" 선택할 수 있음)
-    let result = 1;
-    for (let count of map.values()) {
-        result *= (count + 1);  // 선택할 수 있는 옷 개수 + 1 (고르지 않는 경우 포함)
+    for(let [item, category] of clothes){
+        map[category] = map[category] ? [...map[category], item] : [item];
     }
     
-    // 아무것도 고르지 않는 경우를 제외하기 위해 1을 빼줌
-    return result - 1;
+    console.log(map)
+    
+    Object.values(map).forEach(v=>{
+        answer*=v.length+1
+    })
+    
+    
+    return answer-1;
 }
