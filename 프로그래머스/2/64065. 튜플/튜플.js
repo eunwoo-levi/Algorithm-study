5cf8a99e987d465378119f1ced7a7fb16b8fef93
EunWoo <eunwoo1341@gmail.com>
2025-07-07 20:42:41 +0900

    [level 2] Title: 튜플, Time: 29.96 ms, Memory: 43.4 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,22 +1,24 @@
 function solution(s) {
+    var answer = [];
     
-    const splitArr = s.slice(1,-1).split('},{')
-    splitArr[0]=splitArr[0].slice(1)
-    splitArr[splitArr.length-1] =  splitArr[splitArr.length-1].slice(0,splitArr[splitArr.length-1].length-1)
+    s= s.slice(2, -2).split('},{');
     
-    const arr = splitArr.map(s=>s.split(',').map(Number)).sort((a,b)=>a.length - b.length)
+    s.sort((a,b)=>a.length-b.length)
+    
+    const set = new Set();
     
     
-    const set = new Set()
-    
-    arr.map(a=>{
-        a.map(b=>{
+    for(a of s){
+        a = a.split(',').map(Number);
+        
+        for(b of a){
             if(!set.has(b)){
-                set.add(b)
+                answer.push(b);
+                set.add(b);
             }
-        })
-    })
+                
+        }
+    }
     
-    return [...set]
-    
+    return answer;
 }
