46da83b8f31417d5743596bcc06f6818f6c83ccf
EunWoo <eunwoo1341@gmail.com>
2025-09-03 09:56:18 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 37.94 ms, Memory: 40.7 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,33 +1,33 @@
 function solution(arr) {
-    const res = [0,0];
-    
-    const compress = (x,y, size)=>{
-        const first = arr[y][x];
-        let same = true;
-        
-        for(let i=y; i<y+size; i++){
-            for(let j=x; j<x+size; j++){
-                if(arr[i][j]!==first){
-                    same = false;
-                    break;
-                }
-            }
-            if(!same)   break;
+  const res = [0, 0];
+
+  const compress = (y, x, size) => {
+    const first = arr[y][x];        // <-- 올바른 2차원 인덱싱
+    let same = true;
+
+    // 현재 사각형 [y..y+size), [x..x+size) 범위만 검사
+    for (let i = y; i < y + size; i++) {
+      for (let j = x; j < x + size; j++) {
+        if (arr[i][j] !== first) {  // <-- 올바른 2차원 인덱싱
+          same = false;
+          break;
         }
-        
-        if(same){
-            res[first]++;
-        }
-        else{
-            const half = size/2;
-            compress(x,y,half);
-            compress(x+half,y,half);
-            compress(x,y+half,half);
-            compress(x+half, y+half, half);
-        }
+      }
+      if (!same) break;
     }
-    
-    compress(0,0, arr.length)
-    
-    return res;
+
+    if (same) {
+      res[first]++;
+      return;
+    }
+
+    const half = size >> 1;         // size는 2의 거듭제곱이라 비트연산으로 반 나눠도 OK
+    compress(y, x, half);
+    compress(y, x + half, half);
+    compress(y + half, x, half);
+    compress(y + half, x + half, half);
+  };
+
+  compress(0, 0, arr.length);
+  return res;
 }
