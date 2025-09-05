728253801d460e33a410cd9d08112ee70c45dec4
EunWoo <eunwoo1341@gmail.com>
2025-09-05 11:22:49 +0900

    [level 2] Title: 기능개발, Time: 0.26 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,20 +1,21 @@
 // 스택,큐 & 그리디
 function solution(progresses, speeds) {
-    const days = progresses.map((p, i) => Math.ceil((100 - p) / speeds[i]));
-    const result = [];
-
+    var answer = [];
+    const days = progresses.map((p,index)=> Math.ceil((100-p)/speeds[index]));
+    
     let maxDay = days[0];
     let count = 1;
-
-    for (let i = 1; i < days.length; i++) {
-        if (days[i] <= maxDay) {
-            count++;
-        } else {
-            result.push(count);
-            count = 1;
+    
+    for(let i=1; i<days.length; i++){
+        if(maxDay<days[i]){
             maxDay = days[i];
+            answer.push(count);
+            count = 1;
+            continue;
         }
+        count++;
     }
-    result.push(count);
-    return result;
+    answer.push(count);
+    
+    return answer;
 }
