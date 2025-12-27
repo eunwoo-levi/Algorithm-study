7570d5b334dea4c504dcc285a9ed5a1640d6bde7
EunWoo <eunwoo1341@gmail.com>
2025-12-27 21:40:59 +0900

    [level 1] Title: 모의고사, Time: 29.67 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,27 +1,23 @@
+const first = [1, 2, 3, 4, 5]
+const second = [2, 1, 2, 3, 2, 4, 2, 5]
+const third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
 function solution(answers) {
-    const answer = [];
-    const first = [1, 2, 3, 4, 5];
-    const second = [2, 1, 2, 3, 2, 4, 2, 5];
-    const third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    let result = [];
+    let firstScore=0, secondScore=0, thirdScore=0;
     
-    let res_1 = 0, res_2 = 0, res_3 = 0;
-    const first_len = first.length;
-    const second_len = second.length;
-    const third_len = third.length;
-    
-    for (let i = 0; i < answers.length; i++) {
-        if (first[i % first_len] === answers[i]) res_1++;
-        if (second[i % second_len] === answers[i]) res_2++;
-        if (third[i % third_len] === answers[i]) res_3++;
+    for(let i=0; i<answers.length; i++){
+        const answer = answers[i];
+        if(answer === first[i%first.length])    firstScore++;
+        if(answer === second[i%second.length])  secondScore++;
+        if(answer === third[i%third.length])    thirdScore++;
     }
     
-    const max_res = Math.max(res_1, res_2, res_3);
+    const max = Math.max(firstScore, secondScore, thirdScore);
     
-    [res_1, res_2, res_3].forEach((res, idx) => {
-        if (res === max_res) {
-            answer.push(idx + 1);
-        }
+    [firstScore, secondScore, thirdScore].forEach((s,index)=>{
+        if(s===max) result.push(index+1);
     });
     
-    return answer.sort((a, b) => a - b);
+    return result;
 }
