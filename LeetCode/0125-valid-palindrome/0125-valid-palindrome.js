8a49dd2b02c1d797088453d448456200e82f938f
EunWoo <eunwoo1341@gmail.com>
2025-12-31 21:13:47 +0900

    Time: 20 ms (5.35%), Space: 63.6 MB (10.1%) - LeetHub

--- LeetCode/0125-valid-palindrome/0125-valid-palindrome.js
+++ LeetCode/0125-valid-palindrome/0125-valid-palindrome.js
@@ -1,5 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
 var isPalindrome = function(s) {
-    const k = s.toLowerCase().replace(/[^a-z0-9]/g, "")
-    
-    return k === k.split("").reverse().join("")
+    let str = '';
+
+    for(let i=0; i<s.length;i++){
+        const lowerStr = s[i].toLowerCase();
+        if((lowerStr >= 'a' && lowerStr <= 'z') || (lowerStr >= "0" && lowerStr <= "9") ){
+            str+=lowerStr;
+        }
+    }
+
+
+    const reversedStr = str.split('').reverse().join('');
+
+    console.log(reversedStr)
+
+
+    return str === reversedStr;
 };
