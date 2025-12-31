a7e3f12eb9bd125ea43b2e7926e3cd5750f07718
EunWoo <eunwoo1341@gmail.com>
2025-12-31 20:45:37 +0900

    Time: 1 ms (9.23%), Space: 54.6 MB (58.95%) - LeetHub

--- LeetCode/0088-merge-sorted-array/0088-merge-sorted-array.js
+++ LeetCode/0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -1,14 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
 var merge = function(nums1, m, nums2, n) {
-        
-        let p1 = m-1
-        let p2 = n-1
-        let p3 = m+n-1
-        while(p3 != -1 && p2 != -1) {
-            if (nums1[p1] > nums2[p2]) {
-                nums1[p3--] = nums1[p1--];
-            } else {
-                nums1[p3--] = nums2[p2--];
-            }
-        }
 
+    let k=0;
+    for(let i=m;i<nums1.length;i++){
+        nums1[i] = nums2[k++];
+    }
+
+    nums1.sort((a,b)=>a-b);
+
+    return nums1;
 };
