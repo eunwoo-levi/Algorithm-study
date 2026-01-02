9080ede7f9f7af0ff832b49cc591ebbc4b95a1b1
EunWoo <eunwoo1341@gmail.com>
2026-01-02 18:42:18 +0900

    Time: 54 ms (73.52%), Space: 57.4 MB (89.45%) - LeetHub

--- LeetCode/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
+++ LeetCode/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -1,14 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var removeDuplicates = function(nums) {
-    // If array has 2 or fewer elements, return its length
-    if (nums.length <= 2) return nums.length;
-    
-    let k = 2;
-    
-    for (let i = 2; i < nums.length; i++) {
-        if (nums[i] !== nums[k-2]) {
-            nums[k++] = nums[i];
-        }
+    let k=2;
+    if(nums.length<2){
+        return nums.length;
+    } 
+
+    let index=2;
+
+    for(let i=2; i<nums.length; i++){
+        const cur = nums[i];
+        if(nums[index-2]===cur && nums[index-1]===cur) continue;
+        nums[index++] = cur;
+        k++;
     }
-    
+
     return k;
 };
