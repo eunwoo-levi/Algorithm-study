12ece9c6baec30f7405418bb1848b614b47f0fc8
EunWoo <eunwoo1341@gmail.com>
2026-01-09 20:16:29 +0900

    [level 2] Title: 숫자의 표현, Time: 1.70 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,14 +1,22 @@
-
-//자연수 n을 연속된 자연수의 합으로 표현할 수 있는 경우의 수는
-//👉 n의 홀수 약수의 개수와 같다는 정리!
-
 function solution(n) {
-    var answer = 0;
-    for (var i = 1; i <= n; i++) {
-        if ((n % i == 0) && (i % 2 == 1)) {
-            answer++;
-        }
-    }
-    return answer;
-}
+  let count = 0;
+  let l = 1;
+  let r = 1;
+  let sum = 1;
 
+  while (l <= n) {
+    if (sum === n) {
+      count++;
+      sum -= l;
+      l++;
+    } else if (sum < n) {
+      r++;
+      sum += r;
+    } else { // sum > n
+      sum -= l;
+      l++;
+    }
+  }
+
+  return count;
+}
