39a8b88d322dafdfb70462d7578d52253558a6ef
EunWoo <eunwoo1341@gmail.com>
2026-01-10 19:58:57 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.11 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,9 +1,9 @@
 function solution(n) {
-    const n_one = n.toString(2).split('').filter(a=>a==='1').length;
+    const ones = n.toString(2).split('').filter(a=>a==='1').length;
     let k = n+1;
-    while(1){
-        const k_one = k.toString(2).split('').filter(a=>a==='1').length;
-        if(k_one === n_one) return k;
+    while(k){
+        const anotherOnes = k.toString(2).split('').filter(a=>a==='1').length;
+        if(ones===anotherOnes)  return k;
         k++;
     }
 }
