0a7e8f5d04e51990d1c0fc2cf92f30a1072eade1
EunWoo <eunwoo1341@gmail.com>
2026-01-13 16:29:09 +0900

    [level 3] Title: 스티커 모으기(2), Time: 17.55 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
@@ -1,28 +1,28 @@
-const solution = (sticker) => {
-  const length = sticker.length;
-  if (length === 1) return sticker[0];
-  if (length === 2) return Math.max(sticker[0], sticker[1]);
+function solution(sticker) {
+    var answer = 0;
+    const N = sticker.length;
+    
+    if (N === 1) return sticker[0];
+    if (N === 2) return Math.max(sticker[0], sticker[1]);
 
-  const dp1 = Array(length).fill(0); // 0번 포함 -> 마지막 제외
-  const dp2 = Array(length).fill(0); // 0번 제외 -> 마지막 포함 가능
-
-  dp1[0] = sticker[0];
-  dp1[1] = sticker[0];
-
-  dp2[0] = 0;
-  dp2[1] = sticker[1];
-
-  for (let i = 2; i < length; i++) {
-    // dp1: 마지막은 강제로 선택 불가
-    if (i !== length - 1) {
-      dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + sticker[i]);
-    } else {
-      dp1[i] = dp1[i - 1];
+    const dp1 = Array.from({length: N}, ()=>0);
+    dp1[0] = sticker[0];
+    dp1[1] = sticker[0];
+    const dp2 = Array.from({length: N}, ()=>0);
+    dp2[0] = 0;
+    dp2[1] = sticker[1];
+    
+    for(let i=2; i<N; i++){
+        if(i!==N-1){
+            dp1[i] = Math.max(dp1[i-1], dp1[i-2] + sticker[i]);
+            
+        } else{
+            dp1[i] = dp1[i-1];
+        }
+        
+        dp2[i] = Math.max(dp2[i-1], dp2[i-2] + sticker[i]);
     }
 
-    // dp2: 일반 인접 금지 DP (1..n-1)
-    dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + sticker[i]);
-  }
 
-  return Math.max(dp1[length - 1], dp2[length - 1]);
-};
+    return Math.max(dp1[N-1], dp2[N-1]);
+}
