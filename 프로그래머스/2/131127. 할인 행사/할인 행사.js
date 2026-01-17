b4017c6ab08c95d8da10546d2436b4816146d0e2
Eunwoo <eunwoo1341@gmail.com>
2026-01-17 10:57:17 +0900

    [level 2] Title: 할인 행사, Time: 23.92 ms, Memory: 40 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,28 +1,20 @@
 function solution(want, number, discount) {
-    var answer = 0;
-    
-    const map = {};
-    for (let i = 0; i < want.length; i++) {
-        map[want[i]] = number[i];
-    }
-    
-    // 할인 배열을 10개씩 슬라이싱하며 검사
-    for (let i = 0; i < discount.length - 9; i++) {
-        // map을 복사한 products 객체로 작업
-        const products = { ...map };
-        const slicedDiscount = discount.slice(i, i + 10);
+    let count = 0;
+    for(let i=0; i<discount.length - 9; i++){
+        const sliced = discount.slice(i, i+10);
         
-        // 10일 동안의 제품 할인 처리
-        for (let j = 0; j < 10; j++) {
-            if (products[slicedDiscount[j]] !== undefined) {
-                products[slicedDiscount[j]]--;
+        let flag = true;
+        for(let j=0; j<want.length; j++){
+            const foundLength = sliced.filter(s=>s===want[j]).length;
+            if(foundLength < number[j]){
+                flag = false;
+                break;
             }
         }
         
-        // 모든 제품이 필요한 만큼 다 할인되었는지 체크
-        const res = Object.values(products).every(v => v <= 0);
-        if (res) answer++;
+        if(flag)    count += 1;
     }
     
-    return answer;
+    
+    return count;
 }
