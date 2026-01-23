00beddff2bed239ca2000a05277a331f325e5ed6
Eunwoo <eunwoo1341@gmail.com>
2026-01-23 16:50:48 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.92 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -1,25 +1,30 @@
 function solution(jobs) {
-    /// 들어온 순서대로 나열
+    var answer = 0;
+    const PQ = [];
+    let time=0, i=0;
+    
     jobs.sort((a,b)=>a[0]-b[0]);
-    let answer = 0;
-    let temp = [];
-    const len = jobs.length;
-    /// 시작 시간
-    let counter = 0;
-    while(jobs.length > 0 || temp.length > 0){
-        while(jobs.length > 0 && jobs[0][0]<=counter){
-            temp.push(jobs.shift());
+    
+    while(PQ.length > 0 || i < jobs.length){
+        if(jobs.length > i && jobs[i][0] <= time){
+            PQ.push(jobs[i++]);
+            PQ.sort((a,b)=>a[1]-b[1]);
+            continue;
         }
-        if (temp.length > 0){
-            temp.sort((a,b)=>a[1]-b[1]);
-            const job = temp.shift();
-            answer += ((counter + job[1])-job[0]);
-            counter += job[1];
+        
+        
+        if(PQ.length > 0){
+            const [start, work] = PQ.shift();
+            answer += time - start + work;
+            time += work;
         }
-        else{
-            counter = jobs[0][0];
+        
+        else if(PQ.length == 0){
+            time = jobs[i][0];
         }
     }
     
-    return Math.floor(answer/len);
+    
+    
+    return parseInt(answer/jobs.length);
 }
