319ee3e9e4a5df1db77770abc35fa46b5e0d7e59
Eunwoo <eunwoo1341@gmail.com>
2026-02-03 17:58:27 +0900

    [level 1] Title: 최소직사각형, Time: 6.23 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,12 +1,14 @@
 function solution(sizes) {
-    const max = Math.max(...sizes.flat())
-    let secondMax = 0;
+    let maxA = -1, maxB = -1;
     
-    for(let [x, y] of sizes){
-        const min = Math.min(x,y);
-        if(secondMax<min)   secondMax = min;
+    for(let [a, b] of sizes){
+        if(a<b){
+            [a, b] = [b, a];
+        }
+        
+        if(a>maxA)  maxA = a;
+        if(b>maxB)  maxB = b;
     }
     
-    
-    return max * secondMax;
+    return maxA * maxB;
 }
