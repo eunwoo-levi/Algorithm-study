24bb3960f4a946240eef0bc108e65f7617e15108
Eunwoo <eunwoo1341@gmail.com>
2026-02-03 20:36:38 +0900

    [level 2] Title: 타겟 넘버, Time: 39.69 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,17 +1,18 @@
-// dfs
 function solution(numbers, target) {
     var answer = 0;
+    const N = numbers.length;
     
-    function dfs(length, sum){
-        if(length===numbers.length){
+    function dfs(sum,idx){
+        if(idx===N){
             if(sum===target)    answer++;
             return;
         }
         
-        dfs(length+1, sum+numbers[length]);
-        dfs(length+1, sum-numbers[length]);
+        dfs(sum+numbers[idx], idx+1);
+        dfs(sum-numbers[idx], idx+1);
     }
-    dfs(0,0);
+    
+    dfs(0, 0);
     
     return answer;
 }
