74ba2ee52738dddf6ae5d45bc2fd972aa224a92e
Eunwoo <eunwoo1341@gmail.com>
2026-02-03 19:36:02 +0900

    [level 2] Title: 소수 찾기, Time: 18.82 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,32 +1,40 @@
 function solution(numbers) {
+    var answer = 0;
     const N = numbers.length;
     const set = new Set();
-    const visited = Array.from({length: N+1}, ()=>false);
+    const visited = Array.from({length: N}, ()=> false);
     
-    // 순열
-    function dfs(num){
-        if(num.length>0)    set.add(Number(num));
-        if(num.length===N)    return;
-        
+    // Permutation
+    function dfs(s){
+        if(s.length>0)  set.add(Number(s));
+        if(s.length===N)    return;
+
         for(let i=0; i<N; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                dfs(num + numbers[i]);
-                visited[i] = false;
-            }
+            if(visited[i])  continue;
+            visited[i] = true;
+            dfs(s+numbers[i]);
+            visited[i] = false;
+        }
+    }
+    dfs("")
+    
+    const numberArr = [...set];
+    
+    for(const n of numberArr){
+        if(isPrime(n)){
+            answer++;
         }
     }
     
-    dfs('');
-    
-    return [...set].filter(isPrime).length;
-}
-
-const isPrime = (num) => {
-    if(num<2) return false;
-    for(let i=2; i*i <= num; i++){
-        if(num%i===0)   return false;
+    // 소수 찾기
+    function isPrime(k){
+        if(k<2)   return false;
+        for(let i=2; i*i <= k; i++){
+            if(k%i===0) return false;
+        }
+        
+        return true;
     }
     
-    return true;
-} 
+    return answer;
+}
