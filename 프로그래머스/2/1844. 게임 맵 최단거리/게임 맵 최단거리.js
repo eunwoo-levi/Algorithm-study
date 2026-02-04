16335b338126bc70aeccd5f96c4faf4acbc135a4
Eunwoo <eunwoo1341@gmail.com>
2026-02-05 05:21:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 36.38 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,40 +1,29 @@
-// BFS
-const dy = [-1, 1, 0, 0];
-const dx = [0, 0, -1, 1];
-
+const dy = [1, -1, 0, 0];
+const dx = [0, 0, 1, -1];
 
 function solution(maps) {
     const N = maps.length;
     const M = maps[0].length;
+    const dist = Array.from({length: N}, ()=> Array.from({length: M}, ()=> 0));
+    const queue = [[0, 0]];
+    dist[0][0] = 1;
     
-    const visited = Array.from({length: N},()=> Array.from({length: M}, ()=> 0));
-    
-    
-    function bfs(){
-        const queue = [[0,0]];
-        visited[0][0] = 1;
+    while(queue.length>0){
+        const [y, x] = queue.shift();
         
+        if(y===N-1 && x===M-1){
+            return dist[y][x];
+        }
         
-        while(queue.length){
-            const [y, x] = queue.shift();
+        for(let i=0; i<4; i++){
+            const ny = y + dy[i];
+            const nx = x + dx[i];
             
-            for(let i=0; i<4; i++){
-                const ny = y + dy[i];
-                const nx = x + dx[i];
-
-                if(ny>=0 && ny<N && nx>=0 && nx<M
-                   && visited[ny][nx]===0 && maps[ny][nx]===1 ){
-                    visited[ny][nx] = visited[y][x] + 1;
-                    if(ny===N-1 && nx===M-1){
-                        return visited[ny][nx];
-                    }
-                    
-                    queue.push([ny, nx]);
-                }   
-            }
+            if(ny<0||ny>=N||nx<0||nx>=M||dist[ny][nx]!==0||maps[ny][nx]===0)    continue;
+            dist[ny][nx] = dist[y][x] + 1;
+            queue.push([ny, nx]);
         }
     }
     
-    const res = bfs();
-    return res ?? -1;
+    return -1;
 }
