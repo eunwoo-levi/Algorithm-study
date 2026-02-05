7decb57e119a6f7fbeecb606febb5a4440d14181
Eunwoo <eunwoo1341@gmail.com>
2026-02-05 10:29:40 +0900

    [level 3] Title: 단어 변환, Time: 0.39 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,29 +1,30 @@
 function solution(begin, target, words) {
-    const visited = Array.from({length: words.length}, ()=> false);
+    const N = words.length;
+    const visited = Array.from({length: N}, ()=>false);
     const queue = [[begin, 0]];
     
-    function isValid(A, B){
-        let diff = 0;
-        for(let i=0; i<A.length; i++){
-            if(A[i] !== B[i])   diff++;
-        }
-        
-        return diff === 1;
-    }
-    
-    while(queue.length!==0){
-        const [word, count] = queue.shift();
+    let head = 0;
+    while(head<queue.length){
+        const [word, count] = queue[head++];
         if(word===target)   return count;
         
-        for(let i=0; i<words.length; i++){
+        for(let i=0; i < N; i++){
             if(!visited[i] && isValid(word, words[i])){
                 visited[i] = true;
-                queue.push([words[i], count+1]);
+                queue.push([words[i], count + 1])
             }
         }
     }
     
+    
     return 0;
 }
 
-// BFS <- 최소 단계
+function isValid(A, B){
+    let diff = 0;
+    for(let i=0; i<A.length; i++){
+        if(A[i]!==B[i]) diff++;
+    }
+    
+    return diff === 1;
+}
