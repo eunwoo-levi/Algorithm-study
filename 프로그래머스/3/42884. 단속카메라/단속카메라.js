f81a1252527d4bf7c7ea020ea180117e86a52941
Eunwoo <eunwoo1341@gmail.com>
2026-02-07 00:04:08 +0900

    [level 3] Title: 단속카메라, Time: 7.64 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -1,14 +1,16 @@
 function solution(routes) {
-  let count = 0;
-  let camera = -30001;
-
-  routes.sort((a, b) => a[1] - b[1]);
-
-  for (const [start, end] of routes) {
-    if (start > camera) {
-      count++;
-      camera = end; // 진출 지점에 설치
+    var answer = 0;
+    let camera = -30001
+    
+    routes.sort((a,b)=> a[1] - b[1]);
+    console.log(routes)
+    
+    for(const [start, end] of routes){
+        if(start>camera){
+            camera = end;
+            answer++;
+        }
     }
-  }
-  return count;
+    
+    return answer;
 }
