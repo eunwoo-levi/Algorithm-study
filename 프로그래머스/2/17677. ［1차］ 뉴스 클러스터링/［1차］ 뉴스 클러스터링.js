249930f773c104369ba97a87df6abafecb6bd6ff
Eunwoo <eunwoo1341@gmail.com>
2026-02-07 21:27:09 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.81 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,39 +1,42 @@
-const solution = (str1, str2) => {
-  //각각의 원소 쌍을 구하는 과정 Set으로 구현할 수 없어서 Array를 사용했다.
-  str1 = str1.toUpperCase();
-  str2 = str2.toUpperCase();
-  const arr1 = new Array();
-  const arr2 = new Array();
+// 합집합은 B 에서 A 원소에 속해있으면 해당껄 지우고, for문 마지막에 B에 남은 원소를 다 A에 넣으면 됌
+// 교집합은 위에서 지운것들 모두
 
-  //이부분이 str1과 str2가 겹치므로 함수로 따로 뺐어도 됐을 것 같다.
-  for (let i = 0; i < str1.length - 1; i++) {
-    const str = str1.substr(i, 2);
-    if (str[0] >= "A" && str[0] <= "Z" && str[1] >= "A" && str[1] <= "Z") {
-      arr1.push(str);
-    }
-  }
-  for (let i = 0; i < str2.length - 1; i++) {
-    const str = str2.substr(i, 2);
-    if (str[0] >= "A" && str[0] <= "Z" && str[1] >= "A" && str[1] <= "Z") {
-      arr2.push(str);
-    }
-  }
+function solution(str1, str2) {
+    var answer = 0;
+    const intersection = [], union = [];
+    
+    if(str2.length > str1.length)   [str1, str2] = [str2, str1];
+    str1 = str1.toUpperCase();
+    str2 = str2.toUpperCase();
 
-  //교집합과 합집합을 구하는 과정
-  const intersection = [];
-  const union = [];
-  for (let i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) >= 0) {
-      intersection.push(arr1.splice(arr1.indexOf(arr2[i]), 1));
+    // 유효한 쌍만 필터한 각 배열 생성
+    let arrA = [], arrB = [];
+    for(let i=0; i<str1.length-1; i++){
+        const sliced = str1.slice(i, i+2);
+        if(isAlpha(sliced))    arrA.push(sliced);
     }
-    union.push(arr2[i]);
-  }
+    for(let i=0; i<str2.length-1; i++){
+        const sliced = str2.slice(i, i+2);
+        if(isAlpha(sliced))    arrB.push(sliced);
+    }
+    
+    // union, intersection 구하기
+    for(let i=0; i<arrA.length; i++){
+        const idx = arrB.indexOf(arrA[i]);
+        if(idx!==-1){
+            intersection.push(...arrB.splice(idx, 1));
+        }
+    }
+    union.push(...arrA, ...arrB);
+    
+    if(intersection.length===0 && union.length === 0)   return 65536;
+    return parseInt(intersection.length/union.length * 65536);
+}
 
-  for (let i = 0; i < arr1.length; i++) {
-    union.push(arr1[i]);
-  }
-  if (intersection.length === 0 && union.length === 0) {
-    return 65536;
-  }
-  return parseInt(65536 * (intersection.length / union.length));
-};
+function isAlpha(s){
+    for(let i=0; i<s.length; i++){
+        if('A'> s[i] || s[i] > 'Z')   return false;
+    }
+    
+    return true;
+}
