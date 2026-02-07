7c9e92bb0e8d3013d1694679ad39246c70dc626b
Eunwoo <eunwoo1341@gmail.com>
2026-02-07 09:20:26 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,13 +1,11 @@
-const numbers1 = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-const numbers2 = ["0","1","2","3","4","5","6","7","8","9"]
+const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
 function solution(s) {
     var answer = 0;
     
-    for(let i=0; i<numbers1.length; i++){
-        while(s.includes(numbers1[i])){
-            s = s.replace(numbers1[i], numbers2[i]);
-        }
+    for(let i=0; i<numbers.length; i++){
+        s = s.split(numbers[i]);
+        s = s.join(i);
     }
     
     return Number(s);
