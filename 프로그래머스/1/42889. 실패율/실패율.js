55e5e780aa9de5db4847783de9ca5445ca02f09d
Eunwoo <eunwoo1341@gmail.com>
2026-02-08 03:29:48 +0900

    [level 1] Title: 실패율, Time: 155.52 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,13 +1,36 @@
 function solution(N, stages) {
-    const res = []
+    var answer = [];
     
-    for(let i=1; i<=N; i++){
-        const totalUser = stages.filter((x)=>x>=i).length;
-        const curUser = stages.filter((x)=>x===i).length;
-        res.push([curUser/totalUser, i]);
+    const stageArr = Array.from({length: N+1}, ()=> 0);
+    const failArr = Array.from({length: N+1}, ()=> 0);
+    
+    
+    for(const stage of stages){
+        // 각 스테이지 마다 클리어한 총원 구하기
+        for(let i=1; i<=stage; i++){
+            stageArr[i]++;
+        }
+        
+        // 각 스테이지에 실패한 사람
+        failArr[stage]++;
     }
     
-    res.sort((a,b)=>b[0]-a[0]);
+    // answer: [stage 번호, 실패율]
+    for(let i=1; i<=N; i++){
+        if(stageArr[i] === 0)   answer.push([i, 0]);
+        else {
+            const failPercent = failArr[i]/stageArr[i]
+            answer.push([i, failPercent]);
+        }
+    }
     
-    return res.map((a)=>a[1])
+    console.log('stageArr',stageArr)
+    console.log('failArr',failArr)
+    
+    answer.sort((a,b)=>{
+        if(a[1]===b[1]) return a[0] - b[0];
+        return  b[1] - a[1];
+    })
+    
+    return answer.map(a=>a[0]);
 }
