2cef62f32d0abc0adce3fbd0328c39314c453302
Eunwoo <eunwoo1341@gmail.com>
2026-02-09 21:17:07 +0900

    [level 2] Title: 주차 요금 계산, Time: 4.84 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -1,41 +1,18 @@
 function solution(fees, records) {
-    let res = [];
-    const carSet = new Set();
-    
-    records.forEach(record => {
-        carSet.add(record.split(" ")[1]);
+    const parkingTime = {};
+    records.forEach(r => {
+        let [time, id, type] = r.split(' ');
+        let [h, m] = time.split(':');
+        time = (h * 1) * 60 + (m * 1);
+        if (!parkingTime[id]) parkingTime[id] = 0;
+        if (type === 'IN') parkingTime[id] += (1439 - time);
+        if (type === 'OUT') parkingTime[id] -= (1439 - time);
     });
-    
-    const cars = [...carSet].sort((a, b) => a.localeCompare(b));
-    
-    cars.forEach(car => {
-        const filteredCar = records.filter(record => record.split(" ")[1] === car);
-        
-        if (filteredCar.length % 2 !== 0) {
-            filteredCar.push(`23:59 ${car} OUT`);
-        }
-        
-        let sumFee = 0;
-        let totalMinutes = 0;
-        
-        for (let i = 1; i < filteredCar.length; i += 2) {
-            const outTime = filteredCar[i].split(" ")[0];
-            const inTime = filteredCar[i - 1].split(" ")[0];
-            
-            const outMinutes = Number(outTime.slice(0, 2)) * 60 + Number(outTime.slice(3, 5));
-            const inMinutes = Number(inTime.slice(0, 2)) * 60 + Number(inTime.slice(3, 5));
-            
-            totalMinutes += (outMinutes - inMinutes);
-        }
-
-        if (totalMinutes <= fees[0]) {
-            sumFee = fees[1];
-        } else {
-            sumFee = fees[1] + Math.ceil((totalMinutes - fees[0]) / fees[2]) * fees[3];
-        }
-        
-        res.push(sumFee);
-    });
-
-    return res;
+    const answer = [];
+    for (let [car, time] of Object.entries(parkingTime)) {
+        if (time <= fees[0]) time = fees[1];
+        else time = Math.ceil((time - fees[0]) / fees[2]) * fees[3] + fees[1]
+        answer.push([car, time]);
+    }
+    return answer.sort((a, b) => a[0] - b[0]).map(v => v[1]);
 }
