b5bce54af52bd94005cb5978b4a40fcf53926101
Eunwoo <eunwoo1341@gmail.com>
2026-02-11 07:56:30 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 4.87 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/README.md
+++ 프로그래머스/1/118666. 성격 유형 검사하기/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.56 ms
+메모리: 34.5 MB, 시간: 4.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 07일 13:48:28
+2026년 02월 11일 07:56:28
 
 ### 문제 설명
 
@@ -127,11 +127,12 @@
 <li><code>survey[i]</code>의 첫 번째 캐릭터는 i+1번 질문의 비동의 관련 선택지를 선택하면 받는 성격 유형을 의미합니다. </li>
 <li><code>survey[i]</code>의 두 번째 캐릭터는 i+1번 질문의 동의 관련 선택지를 선택하면 받는 성격 유형을 의미합니다.</li>
 </ul></li>
-<li><p><code>choices</code>의 길이 = <code>survey</code>의 길이</p>
+<li><code>choices</code>의 길이 = <code>survey</code>의 길이
 
 <ul>
 <li><code>choices[i]</code>는 검사자가 선택한 i+1번째 질문의 선택지를 의미합니다.</li>
 <li>1 ≤ <code>choices</code>의 원소 ≤ 7</li>
+</ul></li>
 </ul>
 <table class="table">
         <thead><tr>
@@ -168,9 +169,7 @@
 <td>매우 동의</td>
 </tr>
 </tbody>
-      </table></li>
-</ul>
-
+      </table>
 <hr>
 
 <h5>입출력 예</h5>
--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,16 +1,33 @@
 function solution(survey, choices) {
-    const MBTI = {};
-    const types = ["RT","CF","JM","AN"];
-
-    types.forEach((type) =>
-        type.split('').forEach((char) => MBTI[char] = 0)
-    )
-
-    choices.forEach((choice, index) => {
-        const [disagree, agree] = survey[index];
-
-        MBTI[choice > 4 ? agree : disagree] += Math.abs(choice - 4);
-    });
-
-    return types.map(([a, b]) => MBTI[b] > MBTI[a] ? b : a).join("");
+    var answer = '';
+    
+    const map = new Map();
+    map.set('RT', {'R': 0, 'T': 0}); map.set('CF', {'C': 0, 'F': 0}); map.set('JM', {'J': 0, 'M': 0}); map.set('AN', {'A': 0, 'N': 0});
+    
+    for(let i=0; i<survey.length; i++){
+        const type = survey[i].split('').sort().join('');
+        const choice = choices[i];
+        
+        const obj = map.get(type);
+        
+        let first = type[0];
+        let second = type[1];
+        if(type[0] !== survey[i][0]) [first, second] = [second, first];
+        
+        if(choice<4)    obj[first] += 4-choice;
+        else if(choice>4)   obj[second] += choice-4;
+    }
+    
+    for(const [key, value] of map){
+        const first = key[0];
+        const second = key[1];
+        
+        if(value[first]>value[second]) answer+=first;
+        else if(value[first]<value[second]) answer+=second;
+        else    answer+=first;
+    }
+    
+    console.log(map)
+    
+    return answer;
 }
