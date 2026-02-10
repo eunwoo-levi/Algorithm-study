dec0cf269e720e8b9d104c90fc0b17b180a7ffba
Eunwoo <eunwoo1341@gmail.com>
2026-02-11 06:21:46 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 26.47 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,32 +1,25 @@
-// Slider
 function solution(queue1, queue2) {
-  const n = queue1.length;
-  const queue = queue1.concat(queue2);
-
-  const total = queue.reduce((acc, num) => acc + num, 0);
-  if (total % 2 !== 0) return -1;          // 전체 합이 홀수면 불가
-  const target = total / 2;
-
-  let start = 0;
-  let end = n;                              // 윈도우 [start, end) : 현재 queue1 영역
-  let sum = queue1.reduce((acc, num) => acc + num, 0);
-  let res = 0;
-
-  const LIMIT = n * 3;                      // 충분한 상한
-
-  while (res <= LIMIT) {
-    if (sum === target) return res;
-
-    if (sum > target) {
-      // 왼쪽에서 하나 빼기 (queue1 -> queue2)
-      sum -= queue[start++];
-    } else {
-      // 오른쪽에서 하나 받기 (queue2 -> queue1)
-      if (end >= queue.length) return -1;   // 더 가져올 게 없으면 불가
-      sum += queue[end++];
+    var answer = 0;
+    
+    const queue = queue1.concat(queue2);
+    const total = queue.reduce((acc, cur)=> acc+cur, 0);
+    if(total%2!==0) return -1;  // 전체 합이 홀수면 불가능
+    
+    let l=0, r=queue1.length, target = total/2;
+    let currentSum = queue1.reduce((acc, cur)=>acc+cur, 0);
+    
+    while(r<queue.length){
+        if(currentSum===target)    return answer;
+        
+        else if(currentSum > target){
+            currentSum -= queue[l++];
+        }
+        else if(currentSum < target){
+            currentSum += queue[r++];
+        }
+        
+        answer++;
     }
-    res++;
-  }
-
-  return -1;
+    
+    return -1;
 }
