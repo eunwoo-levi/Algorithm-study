8aa54f6ad32752191ac7853eaab01de943f497aa
Eunwoo <eunwoo1341@gmail.com>
2026-02-14 02:22:53 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 404.62 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,30 +1,33 @@
 function solution(friends, gifts) {
-   const n = friends.length
-   const giftPoints = Array.from({length: n}, ()=>0)
-    const record = Array.from({length: n}, ()=>Array.from({length: n}, ()=>0));
-   const points = Array.from({length: n}, ()=>0)
-   
+    const N = friends.length;
+    
+    const giftPoints = Array.from({length: N}, ()=> 0);
+    const records = Array.from({length: N}, ()=> Array.from({length: N}, ()=> 0));
+    const points = Array.from({length: N}, ()=> 0);
 
+    for(const gift of gifts){
+        const [giver, taker] = gift.split(" ");
+        const giverIndex = friends.indexOf(giver);
+        const takerIndex = friends.indexOf(taker);
+        
+        records[giverIndex][takerIndex] += 1;
+        giftPoints[giverIndex] += 1;
+        giftPoints[takerIndex] -= 1;
+    }
+
+    for(let i=0; i<N; i++){
+        for(let j=0; j<N; j++){
+            if(records[i][j] > records[j][i]){
+                points[i] += 1;
+            }
+            else if(records[i][j]===records[j][i]){
+                if(giftPoints[i] > giftPoints[j]){
+                    points[i] += 1;
+                }
+            }
+        }
+    }
     
-   for(const gift of gifts){
-       const [giver, taker] = gift.split(' ')
-       record[friends.indexOf(giver)][friends.indexOf(taker)] +=1
-       giftPoints[friends.indexOf(giver)] +=1
-       giftPoints[friends.indexOf(taker)] -=1
-   } 
-    
-   for(let i=0;i<n;i++){
-       for(let j=0;j<n;j++){
-           if(record[i][j]>record[j][i]){
-               points[i]+=1
-           } else if(record[i][j]===record[j][i]){
-               if(giftPoints[i]>giftPoints[j]){
-                   points[i]+=1
-               }
-           }
-       }
-   } 
-    return Math.max(...points)
+    return Math.max(...points);
 }
-
 // 시뮬레이션(구현)
