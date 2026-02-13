dd5e3529b07cbad5a4ac0ae8b956590f208223d1
Eunwoo <eunwoo1341@gmail.com>
2026-02-14 03:18:34 +0900

    [level 1] Title: 신고 결과 받기, Time: 448.27 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,26 +1,37 @@
-function solution(id_list, report, k) {
-    var answer = [];
-    
-    const reports = [...new Set(report)].map((a)=>a.split(" "))
-    
-    const bads= new Map();
-    const reporter = new Map();
+// map1 => {key: 신고 당한 유저 ID, value: [신고한 사람 ID] }
+// map2 => {key: 신고 당한 유저 ID,  value: 신고 당한 횟수}
+// map2에서 횟수가 k번 이상일 경우 이것들만 담은 배열을 순환 
+// d=> map1에서 그 key에 해당되는 신고한 사람들을 찾아서 id_list에 indexOf 해서 해당되는 answer 인덱스 ++ 하기
 
-    reports.forEach((v)=>{
-        bads.set(v[1], bads.get(v[1])+1 || 1);      // v[1]: 신고 당한 사람
-    })
+function solution(id_list, report, k) {
+    var answer = Array.from({length: id_list.length}, ()=> 0);
+    const idMap = new Map()
+    const countMap = new Map()
     
-    for(let report of reports){
-        if(bads.get(report[1])>=k){
-            reporter.set(report[0], reporter.get(report[0])+1 || 1)
+    for(const r of report){
+        const [give, get] = r.split(' ');
+        
+        if(!idMap.has(get) || !countMap.has(get)){
+            idMap.set(get, []);
+            countMap.set(get, 0);
         }
+        
+        const userArr = idMap.get(get);
+        if(userArr.includes(give))   continue;
+        
+        idMap.get(get).push(give);
+        countMap.set(get, countMap.get(get)+1)        
     }
     
-    return id_list.map((a)=>reporter.get(a) || 0)
-    
-    
-    
-    
+    for(const [user, count] of countMap){
+        if(count>=k){
+            const reportUsers = idMap.get(user)
+            for(const u of reportUsers){
+                const idx = id_list.indexOf(u);
+                answer[idx]++;
+            }
+        }
+    }
     
     return answer;
 }
