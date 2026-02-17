2ca266e6304d42ea6e20d8652d40854c2abd5f92
Eunwoo <eunwoo1341@gmail.com>
2026-02-17 12:05:26 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 24.30 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -1,64 +1,56 @@
-// 1) 손님마다 고른 메뉴 조합들을 Combination써서 각 length만큼정 갯수 다 뽑음
-// 2) 뽑은 것들을 Map key로 두고 value값들을 하나씩 올림
-// 3) map을 순회하면서 key의 length만큼 분리하고
-// 3-2) 각 length에 해당되는 것들 중 value의 Math.max를 구한 뒤, 해당되는 것들을 answer에 차례대로 push함.
+// map: { key: order, value: order 겹치는 갯수 }
+// maxMap: { key: 각 길이 - course , value: 각 길이에 따른 최댓값}
+
 function solution(orders, course) {
-    const answer = [];
+    var answer = [];
     const map = new Map();
-
-    orders = orders.map(order =>
-        order.split('').sort().join('')
-    );
-
-    for (const order of orders) {
-        for (const len of course) {
-            if (order.length < len) continue;
-
-            const combs = combination(order, len);
-
-            for (const c of combs) {
-                map.set(c, (map.get(c) || 0) + 1);
-            }
-        }
-    }
-
-    for (const len of course) {
-        let max = 0;
-
-        for (const [key, value] of map) {
-            if (key.length === len && value >= 2) {
-                max = Math.max(max, value);
-            }
-        }
-
-        for (const [key, value] of map) {
-            if (key.length === len && value === max && value >= 2) {
-                answer.push(key);
-            }
-        }
-    }
-
+    const maxMap = new Map();
     
-    function combination(order, len){
-        const result = [];
-
-        function dfs(start, cur){
-            if(cur.length===len){
-                result.push(cur);
-                return;
-            }
+    orders = orders.map(order=>order.split('').sort().join(''));
+    
+    for(const order of orders){
+        for(const len of course){
+            const combi = combination(order, len);
             
-            for(let i=start; i<order.length; i++){
-                dfs(i+1, cur+order[i]);
+            for(const c of combi){
+                map.set(c, (map.get(c) || 0) + 1);                 
             }
         }
-        
-        dfs(0, '');
-        return result;
     }
-
-    return answer.sort();
+    
+    // 각 길이(key.length)마다 최댓값 담은 Map
+    for(const [key, value] of map){
+        const len = key.length;
+        if(!maxMap.get(len)) maxMap.set(len, 0);
+        if(maxMap.get(len) < value){
+            if(value<2) continue;
+            maxMap.set(len, value);
+        }
+    }
+    
+    for(const [key, value] of map){
+        const len = key.length;
+        if(maxMap.get(len)===value) answer.push(key);
+    }
+    
+    answer.sort();
+    
+    return answer;
 }
 
+function combination(array, len){
+    const result = [];
+    function dfs(start, cur){
+        if(cur.length===len){
+            result.push(cur)
+            return;
+        }
 
+        for(let i=start; i<array.length; i++){
+            dfs(i + 1, cur + array[i]);
+        }
+    }
 
+    dfs(0, '');
+    return result;
+}
