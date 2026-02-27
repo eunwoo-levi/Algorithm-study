6a071ea167f1a2634b5ebcbbece13eb08436c01e
Eunwoo <eunwoo1341@gmail.com>
2026-02-28 08:53:59 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=python3) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.25 ms
+메모리: 9.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 05일 23:53:04
+2026년 02월 28일 08:53:56
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,18 @@
+def solution(n, lost, reserve):
+    students = [1] * (n + 1)
+
+    for l in lost:
+        students[l] -= 1
+    for r in reserve:
+        students[r] += 1
+
+    for i in range(1, n + 1):
+        if students[i] == 0:
+            if i > 1 and students[i - 1] == 2:
+                students[i - 1] -= 1
+                students[i] += 1
+            elif i < n and students[i + 1] == 2:
+                students[i + 1] -= 1
+                students[i] += 1
+
+    return sum(1 for i in range(1, n + 1) if students[i] > 0)
