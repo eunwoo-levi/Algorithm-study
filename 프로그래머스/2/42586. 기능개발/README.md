b4f9737807b1ae42c0c1e311027a241088189335
Eunwoo <eunwoo1341@gmail.com>
2026-03-14 10:30:17 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 9.18 MB, 시간: 0.04 ms
+메모리: 9.21 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 22일 18:59:34
+2026년 03월 14일 10:30:14
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -2,22 +2,21 @@ import math
 
 def solution(progresses, speeds):
     answer = []
-    days = []
+    
+    max_progress = math.ceil((100 - progresses[0]) / speeds[0])
+    count = 0
     
     for i in range(len(progresses)):
-        days.append(math.ceil((100-progresses[i]) / speeds[i]))
-    
-    count = 1
-    current = days[0]
-    
-    for i in range(1, len(days)):
-        if(current >= days[i]):
+        progress = math.ceil((100 - progresses[i]) / speeds[i])
+        
+        if max_progress >= progress:
             count += 1
         else:
             answer.append(count)
             count = 1
-            current = days[i]
-    
-    answer.append(count)
+            max_progress = progress
+            
+    if count != 0:
+        answer.append(count)
     
     return answer
