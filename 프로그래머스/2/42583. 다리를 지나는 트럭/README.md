a595b9994ad2f54d7b4446dc722000ed24d9b7c9
Eunwoo <eunwoo1341@gmail.com>
2026-03-14 16:54:53 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 92.58 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 9.34 MB, 시간: 53.93 ms
+메모리: 9.3 MB, 시간: 92.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 25일 01:32:23
+2026년 03월 14일 16:54:51
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -3,26 +3,18 @@ from collections import deque
 def solution(bridge_length, weight, truck_weights):
     time = 0
     total = 0
-    bridge = deque([0] * bridge_length)  # 다리 길이만큼 0으로 초기화
-    trucks = deque(truck_weights)
+    index = 0
+    bridge = deque([0] * bridge_length)
 
-    while bridge:
+    while index < len(truck_weights) or total > 0:
         time += 1
-        
-        # 1. 다리에서 하나 빠짐
         total -= bridge.popleft()
-        
-        if trucks:
-            # 2. 다음 트럭을 올릴 수 있으면
-            if total + trucks[0] <= weight:
-                truck = trucks.popleft()
-                bridge.append(truck)
-                total += truck
-            else:
-                bridge.append(0)
-        
-        # 3. 트럭도 없고 다리도 비면 종료
-        if not trucks and total == 0:
-            break
+
+        if index < len(truck_weights) and total + truck_weights[index] <= weight:
+            bridge.append(truck_weights[index])
+            total += truck_weights[index]
+            index += 1
+        else:
+            bridge.append(0)
 
     return time
