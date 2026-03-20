c85760fc970bfee8dc29065084a582e5df5e18fc
Eunwoo <eunwoo1341@gmail.com>
2026-03-20 13:08:22 +0900

    [level 3] Title: 네트워크, Time: 23.09 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,19 @@
 def solution(n, computers):
     answer = 0
-    
     visited = [False] * n
-    
     
     def dfs(node):
         visited[node] = True
         
-        for j in range(len(computers[node])):
-            if not visited[j] and computers[node][j] == 1:
-                dfs(j)
-    
-    for i in range(0, n):
+        for computer in computers[node]:
+            for c in range(n):
+                if not visited[c] and computers[node][c] == 1:
+                    dfs(c)
+         
+    for i in range(n):
         if not visited[i]:
             answer += 1
             dfs(i)
+        
     
     return answer
