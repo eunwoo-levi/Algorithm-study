316069f436c4e7b5bacd8478d854f008fde6c3f3
Eunwoo <eunwoo1341@gmail.com>
2026-03-25 13:48:25 +0900

    [level 3] Title: 입국심사, Time: 625.41 ms, Memory: 13.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,20 @@
+def solution(n, times):
+    answer = 0
+    
+    left = 0
+    right = max(times) * n
+    
+    while left <= right:
+        mid = (left+right) // 2
+        
+        cnt = 0
+        for time in times:
+            cnt += mid // time
+        
+        if cnt >= n:
+            answer = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return answer
