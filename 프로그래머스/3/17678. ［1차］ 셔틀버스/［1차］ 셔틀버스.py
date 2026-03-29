f9f917ff822acbdac747b66f5f754699b77df6c9
Eunwoo <eunwoo1341@gmail.com>
2026-03-29 18:00:20 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 1.17 ms, Memory: 9.52 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
@@ -1,24 +1,29 @@
+# 09:00시부터 t분 간격으로 n번 셔틀
+# 매번 버스 올 때마다 m명씩 태움
+# 만약 꽉차면 제일 마지막 버스에서 다른 사람보다 1분 일찍.
+# timetable에 있는 모든 사람이 다 안타도 상관이없다. -> 매번 도착하는 버스에 "나"가 인원수 안에 먼저 타면 됌
+
 def solution(n, t, m, timetable):
+    answer = 0
     crew = sorted(to_minutes(time) for time in timetable)
-    
+
     bus_time = 9 * 60
     idx = 0
     
     for bus in range(n):
-        count = 0
-        
-        while idx < len(crew) and crew[idx] <= bus_time and count < m:
+        count_crew = 0
+        while idx < len(timetable) and count_crew < m and crew[idx] <= bus_time:
+            count_crew += 1
             idx += 1
-            count += 1
-        
-        if bus == n - 1:
-            if count < m:
+            
+        if bus == n-1:
+            if count_crew < m:
                 answer = bus_time
             else:
-                answer = crew[idx - 1] - 1
+                answer = crew[idx - 1] - 1 
         
         bus_time += t
-    
+        
     return to_hours(answer)
 
 def to_minutes(time_string):
