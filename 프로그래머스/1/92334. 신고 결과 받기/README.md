0266ae731b820ffdd444c76e65dc4b49b5f3c1fc
Eunwoo <eunwoo1341@gmail.com>
2026-04-01 22:07:23 +0900

    [level 1] Title: 신고 결과 받기, Time: 188.66 ms, Memory: 45.7 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/README.md
+++ 프로그래머스/1/92334. 신고 결과 받기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신고 결과 받기 - 92334 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334) 
 
 ### 성능 요약
 
-메모리: 58.9 MB, 시간: 301.23 ms
+메모리: 45.7 MB, 시간: 188.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 02일 17:16:06
+2026년 04월 01일 22:07:19
 
 ### 문제 설명
 
--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,28 +1,20 @@
-# dict: 신고 당한 유저 -> 신고한 유저들 list
-# dict: 신고 당한 유저 -> 신고 당한 횟수 int
 from collections import defaultdict
 
 def solution(id_list, report, k):
     answer = [0] * len(id_list)
+    
     idx = {name: i for i, name in enumerate(id_list)}
-    
-    reported_user = defaultdict(list)
     reported = defaultdict(int)
-    report_set = set()
+    reporters = defaultdict(list)
     
-    for r in report:
-        give, get = r.split(' ')
-        
-        if not f"{give}-{get}" in report_set: 
-            reported_user[get].append(give)
-            reported[get] += 1
-            report_set.add(f"{give}-{get}")
-            
+    for r in set(report):   # 중복 신고 제거
+        give, get = r.split()
+        reported[get] += 1
+        reporters[get].append(give)
     
-    # 신고를 k번 이상 당한 사람 기준으로 => 신고 한 사람들 answer 1씩 올리기
-    for key, value in reported.items():
-        if(value>=k):
-             for user_value in reported_user[key]:
-                    answer[idx[user_value]] += 1
+    for name, cnt in reported.items():
+        if cnt >= k:
+            for reporter in reporters[name]:
+                answer[idx[reporter]] += 1
     
     return answer
