ddb3403428dbc9ce20ea06c142a881d0b79b150c
Eunwoo <eunwoo1341@gmail.com>
2026-05-21 13:09:56 +0900

    [level 2] Title: 소수 찾기, Time: 4.70 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 9.56 MB, 시간: 5.77 ms
+메모리: 9.4 MB, 시간: 4.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 14일 18:56:10
+2026년 05월 21일 13:09:52
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -7,23 +7,27 @@ def solution(numbers):
     nums = set()
     
     for i in range(1, len(numbers)+1):
-        perm = permutations(list(numbers), i)       # permutations은 tuple을 반환함
+        perm = permutations(list(numbers), i)
+        
         for p in perm:
             num = int(''.join(p))
             nums.add(num)
     
-    for num in nums:
-        if isPrime(num):
+    for n in nums:
+        if is_prime(n) == True:
             answer += 1
+    
     
     return answer
 
-def isPrime(n):
-    if n == 0 or n == 1:
+
+def is_prime(n):
+    if n < 2:
         return False
     
     for i in range(2, int(math.sqrt(n))+1):
-        if n%i == 0:
+        if n % i == 0:
             return False
     
     return True
+        
