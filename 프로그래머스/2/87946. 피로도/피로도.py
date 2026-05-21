9cf463cbb8a32438e5f486589e776e7c5afd9ef3
Eunwoo <eunwoo1341@gmail.com>
2026-05-21 17:26:45 +0900

    [level 2] Title: 피로도, Time: 28.77 ms, Memory: 9.02 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,6 +1,7 @@
 def solution(k, dungeons):
     answer = 0
     N = len(dungeons)
+    
     visited = [False] * N
     
     def dfs(k, cnt):
@@ -10,12 +11,11 @@ def solution(k, dungeons):
         
         for i in range(N):
             need, cost = dungeons[i]
+            
             if not visited[i] and k >= need:
                 visited[i] = True
                 dfs(k - cost, cnt + 1)
                 visited[i] = False
-                
     dfs(k, 0)
-    
     
     return answer
